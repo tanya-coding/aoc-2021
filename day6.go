94cce5b657f1cb8235eece60734b0f9671df1f4e
tanya <ninjinsky101@gmail.com>
2021-12-06 19:12:50 -0600

    Day 6 - Part 2 Use efficient storage so we can actually calculate result

--- day6.go
+++ day6.go
@@ -6,21 +6,41 @@ import (
 	"strings"
 )
 
-func multiply(fishes []int, days int) []int {
+// Convert simple array of fishes timers into a short array
+// where each item represents a bucket of fishes with the same
+// timer, e.g.
+// 0: num-fishes-with-timer-0
+// 1: num-fishes-with-timer-1
+// ...
+// 8: num-fishes-with-timer-8
+func compact(fishes []int) [9]int {
+	compactFishes := [9]int{}
+	for _, f := range fishes {
+		compactFishes[f]++
+	}
+	return compactFishes
+}
+
+func multiply(fishes [9]int, days int) [9]int {
 	for d := 1; d <= days; d++ {
-		newFishes := []int{}
 		// fmt.Println("Begin day:", fishes)
-		for f := 0; f < len(fishes); f++ {
-			fishes[f]--
-			if fishes[f] < 0 {
-				fishes[f] = 6
-				newFishes = append(newFishes, 8)
-			}
+		reproduced := fishes[0]
+		for gen := 1; gen < len(fishes); gen++ {
+			fishes[gen-1] = fishes[gen]
 		}
-		fishes = append(fishes, newFishes...)
+		fishes[8] = reproduced
+		fishes[6] += reproduced
 		// fmt.Println("End day:", fishes)
 	}
 	return fishes
+}
+
+func countFishes(fishes [9]int) int {
+	total := 0
+	for _, c := range fishes {
+		total += c
+	}
+	return total
 }
 
 func slurpDay6(path string) ([]int, error) {
@@ -42,6 +62,6 @@ func day6() {
 	if err != nil {
 		panic(err)
 	}
-	newFishes := multiply(fishes, 80)
-	fmt.Println(len(newFishes))
+	generations := multiply(compact(fishes), 256)
+	fmt.Println(countFishes(generations))
 }
