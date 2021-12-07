bae6543fffca8fbdf71dd64d769bc3b9139d2f5c
tanya <ninjinsky101@gmail.com>
2021-12-06 19:15:41 -0600

    Clean up

--- day6.go
+++ day6.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 )
 
-// Convert simple array of fishes timers into a short array
-// where each item represents a bucket of fishes with the same
+// Convert simple array of fish timers into a short array
+// where each item represents a geneation of fishes with the same
 // timer, e.g.
 // 0: num-fishes-with-timer-0
 // 1: num-fishes-with-timer-1
@@ -21,23 +21,21 @@ func compact(fishes []int) [9]int {
 	return compactFishes
 }
 
-func multiply(fishes [9]int, days int) [9]int {
+func multiply(generations [9]int, days int) [9]int {
 	for d := 1; d <= days; d++ {
-		// fmt.Println("Begin day:", fishes)
-		reproduced := fishes[0]
-		for gen := 1; gen < len(fishes); gen++ {
-			fishes[gen-1] = fishes[gen]
+		reproduced := generations[0] // How many will produce offspring this day
+		for gen := 1; gen < len(generations); gen++ {
+			generations[gen-1] = generations[gen] // Promote generation
 		}
-		fishes[8] = reproduced
-		fishes[6] += reproduced
-		// fmt.Println("End day:", fishes)
+		generations[8] = reproduced  // New offspring
+		generations[6] += reproduced // Reset timer of fishes that produced offspring
 	}
-	return fishes
+	return generations
 }
 
-func countFishes(fishes [9]int) int {
+func countFishes(generations [9]int) int {
 	total := 0
-	for _, c := range fishes {
+	for _, c := range generations {
 		total += c
 	}
 	return total
